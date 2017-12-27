a3f6d7c30d1a6df6d9b9a4b3b09f0705b60d3687
Patryk Pazura <patryk.pazura@gmail.com>
2017-12-27 23:18:04 +0100

    some changes in finished and working data.js + added approach to send variable from data.js to script.js

--- data.js
+++ data.js
@@ -1,7 +1,21 @@
-const url = 'https://pastebin.com/9yXnuQwZ';
-const correctUrl = [url.slice(0, 21), 'raw/', url.slice(21)].join('');
-fetch(correctUrl)
-    .then((resp) => resp.json())
-    .then(function(data) {
-        console.log(data); 
-    });
+function getData(url) {  
+    const correctUrl = [url.slice(0, 21), 'raw/', url.slice(21)].join('');
+
+    return fetch(correctUrl, 
+        {
+            method: "GET",
+            headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            }
+        })
+        .then((response) => response.json())
+        .then((responseData) => {
+            return responseData;
+        })
+        .catch(error => console.warn(error));
+}
+
+export var movies = [];
+getData('https://pastebin.com/9yXnuQwZ')
+    .then(responseData => { movies.push(responseData)});
---
+++ script.js
@@ -0,0 +1,5 @@
+import { movies } from './data.js';
+
+
+
+     
